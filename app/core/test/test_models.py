d8d9ddc52290ada023c5fc47218affe901c37d5d
mohammad azimi <azimi.jozani.mohammad@gmail.com>
2023-01-09 11:27:50 +0330

    create unitest for my user model

---
+++ app/core/test/test_models.py
@@ -0,0 +1,21 @@
+"""
+tests for models
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """ test models """
+
+    def test_create_user_with_email_successful(self):
+        email = 'uesr@egample.com'
+        password = 'password123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+
+        )
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
