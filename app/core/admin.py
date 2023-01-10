c0dd31fb0a58fa4be908e0a253d63d5bad5e8e53
mohammad azimi <azimi.jozani.mohammad@gmail.com>
2023-01-10 10:03:42 +0330

    write test for create and edit in admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,9 +3,39 @@ django admin customization
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import ugettext_lazy as _
 from . import models
 
 @admin.register(models.User)
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    "is_staff",
+                    "is_superuser"
+                )
+            }
+        ),
+        (_("Important Date"), {'fields': ('last_login', )}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
