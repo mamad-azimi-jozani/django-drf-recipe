c5ff8e789a9f49fc3a15e9626d66271311eb41d4
mohammad azimi <azimi.jozani.mohammad@gmail.com>
2023-01-10 09:06:16 +0330

    register admin_user and test pass

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin # noqa
+"""
+django admin customization
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from . import models
 
-# Register your models here.
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
