d989501ba084af73c544121140badd3e6d4eaaca
mohammad azimi <azimi.jozani.mohammad@gmail.com>
2023-01-10 08:55:54 +0330

    write test for the django admin

---
+++ app/core/test/test_admin.py
@@ -0,0 +1,30 @@
+"""
+test for the django admin
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='pass123123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@gmail.com',
+            password='123qwe123',
+            name='Test User'
+        )
+    def test_users_list(self):
+        """ test that user listed on page """
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
